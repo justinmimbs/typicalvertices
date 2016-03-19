8106409fd6043716858ce903f4a7f5bf0c94d2f8
Justin Mimbs <justin.mimbs@gmail.com>
2016-03-19 01:44:01 -0400

    Float diagrams for a fluid layout

--- app/index.js
+++ app/index.js
@@ -9,12 +9,12 @@ function pathDataFromCoordinates(coordinates) {
     .join('L')}`;
 }
 
-// [Number] -> Element
-function domFromVertexConfiguration(ns) {
-  return dom.g(null,
-    dom.text({ transform: 'translate(1.4, -1.4)' }, ns.slice().reverse().join(',')),
+// Object -> Element
+function domVertexConfiguration({ transform, config }) {
+  return dom.g({ transform },
+    dom.text({ transform: 'translate(1.4, -1.4)' }, config.slice().reverse().join(',')),
     dom.circle({ r: 0.5 }),
-    ns.reduce((result, n) => {
+    config.reduce((result, n) => {
       const d = pathDataFromCoordinates(vertexCoordinates(n));
       result.paths.push(dom.path({ d, transform: `rotate(${result.angle})` }));
       result.angle += internalAngle(n);
@@ -23,27 +23,15 @@ function domFromVertexConfiguration(ns) {
   );
 }
 
-// (Object, [Node]) -> Element
-function domGrid({ transform, rows, cellWidth, cellHeight }, children) {
-  return dom.g({ transform: transform || '' }, children.map((node, i) => {
-    const c = i % rows;
-    const r = Math.floor(i / rows);
-    return dom.g({ transform: `translate(${c * cellWidth}, ${r * cellHeight})` }, node);
-  }));
-}
-
 // create display
 
-const svg = dom.svg({ width: 1650, height: 1050 },
-  domGrid(
-    {
-      transform: 'translate(120.5, 120.5) scale(30, 30)',
-      rows: 7,
-      cellWidth: 7,
-      cellHeight: 8,
-    },
-    typicalVertices.map(domFromVertexConfiguration)
+const div = dom.div(null,
+  typicalVertices.map(config =>
+    dom.div({ className: 'cell' },
+      dom.svg(null,
+        domVertexConfiguration({ transform: 'translate(100.5, 110.5) scale(30, 30)', config }))
+    )
   )
 );
 
-document.querySelector('body').insertBefore(svg, document.querySelector('body').firstChild);
+document.querySelector('#output').appendChild(div);
